d241bee82ec5723655432151e42ab2ee8a4fed3a
jordan rhea <rheajt@gmail.com>
2020-05-10 15:08:00 +0800

    added a config.fish file

---
+++ config.fish
@@ -0,0 +1,13 @@
+# https://fishshell.com/docs/current/tutorial.html#tutorial
+
+# aliases
+alias vim='nvim'
+
+# abbreviations
+if status --is-interactive
+    abbr --add --global gc 'git add -A; git commit -am'
+    abbr --add --global second 'echo my second abbreviation'
+    abbr --add --global tas 'tmux attach-session -t'
+    abbr --add --global tds 'tmux detach'
+    abbr --add --global tns 'tmux new -s'
+end
