d46ec038f7286de426baaab7b867fc21e940c35d
jordan rhea <rheajt@gmail.com>
2020-05-10 15:40:07 +0800

    added a global abbr for 'git status'

--- config.fish
+++ config.fish
@@ -5,6 +5,7 @@ alias vim='nvim'
 
 # abbreviations
 if status --is-interactive
+    abbr --add --global gs 'git status'
     abbr --add --global gc 'git add -A; git commit -am'
     abbr --add --global second 'echo my second abbreviation'
     abbr --add --global tas 'tmux attach-session -t'
