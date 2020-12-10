76f9817ef44882d14178d486cfda8235ba3c4f6f
jordan rhea <rheajt@gmail.com>
2020-12-10 10:09:38 +0800

    updated gcgp.fish with description

--- functions/gcgp.fish
+++ functions/gcgp.fish
@@ -1,4 +1,4 @@
-function gcgp
+function gcgp --description "git add -A; git commit ...; git push ..."
     echo "commit " $argv[1] " into " $argv[2] " branch"
     command git add -A
     command git commit -am $argv[1]
