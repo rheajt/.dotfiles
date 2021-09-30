2093c05dbf4cb86520ced4b2b09b9bf9e45525ce
jordan rhea <rheajt@gmail.com>
2021-09-30 12:05:10 +0800

    added fzf_key_bindings

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 # https://fishshell.com/docs/current/tutorial.html#tutorial
+fzf_key_bindings
 
 # dangerous specific
 set dangerous_colors 000000 333333 666666 ffffff ffff00 ff6600 ff0000 ff0033 3300ff 0000ff 00ffff 00ff00
