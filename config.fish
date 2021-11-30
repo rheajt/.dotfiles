941a3dc93df2fc3c486b8b78e395ecfd0da79d40
jordan rhea <rheajt@gmail.com>
2021-11-30 11:31:01 +0800

    added some tmux abbreviations

--- config.fish
+++ config.fish
@@ -38,6 +38,8 @@ if status --is-interactive
   abbr --add --global tas 'tmux attach-session -t'
   abbr --add --global tds 'tmux detach'
   abbr --add --global tns 'tmux new -s'
+  abbr --add --global tl 'tmux ls'
+  abbr --add --global tks 'tmux kill-session -t'
 
   ### folders
   abbr --add --global pj '~/projects'
