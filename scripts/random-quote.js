06136157f367984a67b3473c81d4681b769e68ce
jordan rhea <rheajt@gmail.com>
2021-12-05 10:50:03 +0800

    changed random-quotes.js

--- scripts/random-quote.js
+++ scripts/random-quote.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function getQuote() {
-  const quotes = fs.readFileSync(__dirname + '/quotes', 'UTF-8').split('\n').filter(ln => ln !== '');
-  const random = Math.floor(Math.random() * quotes.length + 1);
+  const quotes = fs
+    .readFileSync(__dirname + "/quotes", "UTF-8")
+    .split("\n")
+    .filter((ln) => ln !== "");
+  const random = Math.floor(Math.random() * quotes.length);
   console.log(quotes[random]);
 }
 
 getQuote();
-
