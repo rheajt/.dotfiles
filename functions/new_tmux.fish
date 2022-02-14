1437897cae36b54945d9dd41c0b83ed98d2623c6
jordan rhea <rheajt@gmail.com>
2022-02-14 17:11:13 +0800

    updated the tmux functions

--- functions/new_tmux.fish
+++ functions/new_tmux.fish
@@ -3,14 +3,14 @@ function new_tmux
     tmux new-session -s $DIR_STEM -n DEV_CONSOLE -d
     tmux new-window -t $DIR_STEM -d -n CODE
 
-    tmux send-keys -t $DIR_STEM:CODE  "nvim"
+    tmux send-keys -t $DIR_STEM:CODE  "nvim" Enter
 
     if test -f readme.md
-        tmux send-keys -t $DIR_STEM:DEV_CONSOLE "bat readme.md"
+        tmux send-keys -t $DIR_STEM:DEV_CONSOLE "bat readme.md" Enter
     else if test -f readme.text
-        tmux send-keys -t $DIR_STEM:DEV_CONSOLE "bat readme.txt"
+        tmux send-keys -t $DIR_STEM:DEV_CONSOLE "bat readme.txt" Enter
     else if test -f README.md
-        tmux send-keys -t $DIR_STEM:DEV_CONSOLE "bat README.md"
+        tmux send-keys -t $DIR_STEM:DEV_CONSOLE "bat README.md" Enter
     end
 
     tmux select-window -t $DIR_STEM:DEV_CONSOLE
