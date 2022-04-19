a11492d5915fa957de7b2119b6871b328f055c2c
jordan rhea <rheajt@gmail.com>
2022-04-19 10:48:45 +0800

    restructure nvim_cmp

--- nvim/lua/config/lsp/settings/gopls.lua
+++ nvim/lua/config/lsp/settings/gopls.lua
@@ -1,3 +1,17 @@
-local opts = {}
+local util = require("lspconfig/util")
+
+local opts = {
+	cmd = { "gopls", "serve" },
+	filetypes = { "go", "gomod" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+		},
+	},
+}
 
 return opts
