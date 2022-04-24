522e8b97a49c7afe5561b6600bb63d6bbd484f52
jordan rhea <rheajt@gmail.com>
2022-04-25 07:31:18 +0800

    gofmt  as null-ls formatting

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -3,6 +3,7 @@ local null_ls = require("null-ls")
 local sources = {
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.prettierd,
+	null_ls.builtins.formatting.gofmt,
 	null_ls.builtins.diagnostics.tsc,
 	null_ls.builtins.completion.luasnip,
 }
