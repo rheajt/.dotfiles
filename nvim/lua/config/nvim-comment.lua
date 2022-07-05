a012c97c0a90c75e45701bbdd8c80cc7d3611db8
jordan rhea <rheajt@gmail.com>
2022-07-06 07:56:29 +0800

    fix: accidentally removed my comment keymaps

--- nvim/lua/config/nvim-comment.lua
+++ nvim/lua/config/nvim-comment.lua
@@ -2,8 +2,8 @@ local nnoremap = require("config.keymap_binds").nnoremap
 local vnoremap = require("config.keymap_binds").vnoremap
 
 -- NVIM COMMENT
-nnoremap("<leader>/", "gcc")
-vnoremap("<leader>/", "gc")
+nnoremap("<leader>/", ":CommentToggle<CR>")
+vnoremap("<leader>/", ":'<,'>CommentToggle<CR>")
 
 require("nvim_comment").setup({
 	comment_empty = false,
