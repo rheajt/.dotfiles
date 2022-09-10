c31ddcaaa8cc113a70e5c820ded292cf9fc9e085
jordan rhea <rheajt@gmail.com>
2022-09-10 08:50:49 +0800

    updated null-ls to include esline diagnostics

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -5,6 +5,7 @@ local sources = {
 	null_ls.builtins.formatting.prettierd,
 	null_ls.builtins.formatting.gofmt,
 	null_ls.builtins.diagnostics.tsc,
+	null_ls.builtins.diagnostics.eslint,
 	null_ls.builtins.completion.luasnip,
 }
 
