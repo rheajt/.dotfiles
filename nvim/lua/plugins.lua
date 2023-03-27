31d35db7b3bcf474bb962f5a5f356e85ce643955
jordan rhea <rheajt@gmail.com>
2023-03-27 10:37:22 +0800

    trying to get scratchpads working

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -217,7 +217,21 @@ require("lazy").setup({
         "glepnir/lspsaga.nvim",
         event = "BufRead",
         config = function()
-            require("lspsaga").setup({})
+            require("lspsaga").setup({
+                diagnostic = {
+                    on_insert = false,
+                },
+                lightbulb = {
+                    enable = false,
+                },
+                code_action = {
+                    num_shortcut = true,
+                    keys = {
+                        quit = "q",
+                        exec = "<CR>",
+                    },
+                },
+            })
         end,
         dependencies = {
             -- { "nvim-tree/nvim-web-devicons" },
