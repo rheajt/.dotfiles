f537e337bd5ff1cce2b49b1344084e4bdd3e4910
jordan rhea <rheajt@gmail.com>
2023-05-06 13:30:51 +0800

    updated the tray polybar location

--- polybar-launch.sh
+++ polybar-launch.sh
@@ -11,7 +11,12 @@ polybar-msg cmd quit
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --config=~/.config/polybar/config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
+        # print 'main' if m = 'eDP-1'
+        if [ "$m" == "eDP-1" ]; then
+            MONITOR=$m polybar --config=~/projects/dotfiles/polybar-config-main.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
+        else
+            MONITOR=$m polybar --config=~/projects/dotfiles/polybar-config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
+        fi
     done
 else
     polybar --config=~/.config/polybar/config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
