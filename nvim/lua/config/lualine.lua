8b51dd02af690e559113ad180a6efd5328a4e5d0
jordan rhea <rheajt@gmail.com>
2023-06-01 08:23:46 +0800

    still tinkering with settings

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,9 +1,9 @@
 return require("lualine").setup({
     options = {
         icons_enabled = true,
-        theme = "gruvbox_dark",
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        theme = "gruvbox-baby",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         -- component_separators = { left = '', right = ''},
         -- section_separators = { left = '', right = ''},
         disabled_filetypes = { "NvimTree" },
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,6 +1,6 @@
 vim.opt.cursorline = true
 
-vim.g.gruvbox_baby_function_style = "NONE"
+-- vim.g.gruvbox_baby_function_style = "NONE"
 vim.g.gruvbox_baby_keyword_style = "italic"
 vim.g.gruvbox_baby_highlights = { Normal = { fg = "#123123", bg = "NONE", style = "underline" } }
 vim.g.gruvbox_baby_telescope_theme = 1
