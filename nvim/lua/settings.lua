c6ece2f6ef9a2093235de8af6d04424dacdf1f48
jordan rhea <rheajt@gmail.com>
2023-07-22 16:38:05 +0800

    removed the smart indent because i hated it

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -35,7 +35,8 @@ vim.opt.expandtab = true
 vim.opt.smarttab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.smartindent = true
+vim.opt.autoindent = true
+-- vim.opt.smartindent = true
 
 vim.opt.cmdheight = 2
 
