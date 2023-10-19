d515c3f96b70728fc0e7b0575107b7ca48f271a3
jordan rhea <rheajt@gmail.com>
2023-10-19 11:56:31 +0800

    lock screen

--- polybar-launch.sh
+++ polybar-launch.sh
@@ -13,7 +13,7 @@ if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         # print 'main' if m = 'eDP-1'
         if [ "$m" == "eDP-1" ]; then
-            MONITOR=$m polybar --config=~/projects/dotfiles/polybar-config.ini main 2>&1 | tee -a /tmp/polybar1.log & disown
+            MONITOR=$m polybar --config=~/projects/dotfiles/polybar-config.ini main &> /dev/null | tee -a /tmp/polybar1.log & disown
         else
             MONITOR=$m polybar --config=~/projects/dotfiles/polybar-config.ini attached 2>&1 | tee -a /tmp/polybar1.log & disown
         fi
