393bdb565d7c405ea3be9291d00514fe2ff93547
jordan rhea <rheajt@gmail.com>
2024-07-26 13:20:56 +0800

    added treesitter-context

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
+		lazy = true,
 		config = function()
 			local telescope = require("telescope")
 			local actions = require("telescope.actions")
@@ -165,6 +166,7 @@ return {
 	},
 	{
 		"nvim-telescope/telescope-file-browser.nvim",
+		lazy = true,
 		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	},
 }
