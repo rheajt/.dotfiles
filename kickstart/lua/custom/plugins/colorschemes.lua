76940596f30858ccb43f762df27bf5a42e70fdf5
jordan rhea <rheajt@gmail.com>
2024-10-22 10:41:42 +0800

    updating the colorscheme to try tokyonight

--- kickstart/lua/custom/plugins/colorschemes.lua
+++ kickstart/lua/custom/plugins/colorschemes.lua
@@ -1,20 +1,22 @@
 return {
 	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"folke/tokyonight.nvim",
-		-- priority = 1000, -- Make sure to load this before all the other start plugins.
-		-- init = function()
-		-- Load the colorscheme here.
-		-- Like many other themes, this one has different styles, and you could load
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		-- vim.cmd.colorscheme("tokyonight-night")
-
-		-- You can configure highlights by doing something like:
-		-- vim.cmd.hi("Comment gui=none")
-		-- end,
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		opts = {
+			transparent = true,
+			styles = {
+				floats = "transparent",
+				sidebars = "transparent",
+			},
+		},
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-storm")
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
 	},
 	{
 		"samharju/synthweave.nvim",
@@ -34,23 +36,23 @@ return {
 	},
 	{
 		"luisiacc/gruvbox-baby",
-		priority = 1000,
-		init = function()
-			vim.g.gruvbox_baby_keyword_style = "italic"
-			vim.g.gruvbox_baby_telescope_theme = 1
-			vim.g.gruvbox_baby_background_color = "dark"
-			vim.g.gruvbox_baby_function_style = "NONE"
-			vim.g.gruvbox_baby_transparent_mode = 1
-
-			vim.cmd.colorscheme("gruvbox-baby")
-			vim.api.nvim_set_hl(0, "Normal", {
-				bg = "none",
-			})
-
-			vim.api.nvim_set_hl(0, "NonText", {
-				bg = "none",
-			})
-		end,
+		-- priority = 1000,
+		-- init = function()
+		-- 	vim.g.gruvbox_baby_keyword_style = "italic"
+		-- 	vim.g.gruvbox_baby_telescope_theme = 1
+		-- 	vim.g.gruvbox_baby_background_color = "dark"
+		-- 	vim.g.gruvbox_baby_function_style = "NONE"
+		-- 	vim.g.gruvbox_baby_transparent_mode = 1
+		--
+		-- 	vim.cmd.colorscheme("gruvbox-baby")
+		-- 	vim.api.nvim_set_hl(0, "Normal", {
+		-- 		bg = "none",
+		-- 	})
+		--
+		-- 	vim.api.nvim_set_hl(0, "NonText", {
+		-- 		bg = "none",
+		-- 	})
+		-- end,
 	},
 	"sekke276/dark_flat.nvim",
 	"rebelot/kanagawa.nvim",
