5b5ae8c0143de29fa9e0e819795e4876afa3e1ef
jordan rhea <rheajt@gmail.com>
2025-04-14 09:48:20 +0800

    updated the colorscheme

--- kickstart/lua/custom/plugins/colorschemes.lua
+++ kickstart/lua/custom/plugins/colorschemes.lua
@@ -3,13 +3,13 @@ return {
 		"luisiacc/gruvbox-baby",
 		priority = 1000,
 		init = function()
-			vim.g.gruvbox_baby_keyword_style = "italic"
-			vim.g.gruvbox_baby_telescope_theme = 1
-			vim.g.gruvbox_baby_background_color = "dark"
-			vim.g.gruvbox_baby_function_style = "NONE"
-			vim.g.gruvbox_baby_transparent_mode = 1
+			-- vim.g.gruvbox_baby_keyword_style = "italic"
+			-- vim.g.gruvbox_baby_telescope_theme = 1
+			-- vim.g.gruvbox_baby_background_color = "dark"
+			-- vim.g.gruvbox_baby_function_style = "NONE"
+			-- vim.g.gruvbox_baby_transparent_mode = 1
 
-			vim.cmd.colorscheme("gruvbox-baby")
+			-- vim.cmd.colorscheme("gruvbox-baby")
 			-- vim.api.nvim_set_hl(0, "Normal", {
 			-- 	bg = "none",
 			-- })
@@ -20,4 +20,15 @@ return {
 		end,
 	},
 	{ "rebelot/kanagawa.nvim", lazy = true },
+	{
+		"srt0/everblush.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("everblush").setup({
+				transparent = true, -- Set to true for transparency
+			})
+			vim.cmd.colorscheme("everblush")
+		end,
+	},
 }
