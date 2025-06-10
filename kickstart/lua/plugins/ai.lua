e9345d0dad80de2b28351318b6971f0578fa2429
jordan rhea <rheajt@gmail.com>
2025-06-10 11:29:01 +0800

    updated the ai again

--- kickstart/lua/plugins/ai.lua
+++ kickstart/lua/plugins/ai.lua
@@ -36,9 +36,8 @@ return {
 				openai = {
 					endpoint = "https://api.openai.com/v1",
 					model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-					timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+					timeout = 30000,
 					extra_request_body = {
-						timeout = 30000,
 						temperature = 0.75,
 						max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
 					},
