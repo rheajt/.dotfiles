5a2cfa8ae6e581e4da795caaef0ac0d0d8c1d995
jordan rhea <rheajt@gmail.com>
2025-07-24 14:17:51 +0800

    updated the alias files

--- kickstart/lua/plugins/ai.lua
+++ kickstart/lua/plugins/ai.lua
@@ -195,7 +195,6 @@ return {
         keys = {
             -- opencode.nvim exposes a general, flexible API — customize it to your workflow!
             -- But here are some examples to get you started :)
-            { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle opencode', },
             { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = { 'n', 'v' }, },
             { '<leader>oA', function() require('opencode').ask('@file ') end, desc = 'Ask opencode about current file', mode = { 'n', 'v' }, },
             { '<leader>on', function() require('opencode').command('/new') end, desc = 'New session', },
