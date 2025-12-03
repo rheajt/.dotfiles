d2a58319dd0878b78492f5849428224acaa8c722
jordan rhea <rheajt@gmail.com>
2025-12-03 16:20:11 +0800

    updated the sidekick settings

--- kickstart/lua/plugins/ai.lua
+++ kickstart/lua/plugins/ai.lua
@@ -77,10 +77,21 @@ return {
 				"<tab>",
 				function()
 					-- if there is a next edit, jump to it, otherwise apply it if any
-					if not require("sidekick").nes_jump_or_apply() then
-						return "<Tab>" -- fallback to normal tab
+					if require("sidekick").nes_jump_or_apply() then
+						return -- jumped or applied
 					end
+
+					-- if you are using Neovim's native inline completions
+					if vim.lsp.inline_completion.get() then
+						return
+					end
+
+					-- any other things (like snippets) you want to do on <tab> go here.
+
+					-- fall back to normal tab
+					return "<tab>"
 				end,
+				mode = { "i", "n" },
 				expr = true,
 				desc = "Goto/Apply Next Edit Suggestion",
 			},
@@ -141,7 +152,10 @@ return {
 			{
 				"<leader>ap",
 				function()
-					require("sidekick.cli").prompt()
+					local opts = {
+						prompts = prompts,
+					}
+					require("sidekick.cli").prompt(opts)
 				end,
 				mode = { "n", "x" },
 				desc = "Sidekick Select Prompt",
