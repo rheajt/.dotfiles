2c3558192997c167539cee99e4f00b84c01b3b71
jordan rhea <rheajt@gmail.com>
2026-04-26 13:16:26 +0800

    updated obsidian to use templates

--- twelve/plugin/obsidian.lua
+++ twelve/plugin/obsidian.lua
@@ -17,10 +17,17 @@ defer_plugin({
 			picker = {
 				name = "blink",
 			},
+			templates = {
+				folder = "templates", -- Folder where templates are stored
+				date_format = "YYYY-MM-DD", -- Date format for substitutions
+				time_format = "HH:mm", -- Time format for substitutions
+				substitutions = {}, -- Custom substitutions if needed
+			},
 			daily_notes = {
 				folder = "01_journal",
 				date_format = "%Y-%m-%d",
 				default_tags = { "journal" },
+				template = "daily.md",
 			},
 		})
 		vim.keymap.set("n", "<leader><CR>", require("obsidian.api").smart_action, { buffer = true })
