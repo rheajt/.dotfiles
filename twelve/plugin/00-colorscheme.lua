3619e8d13612bc5cb48738540bf05511d3173da7
jordan rhea <rheajt@gmail.com>
2026-06-25 11:00:05 +0800

    removed the twelve neovim customization

--- twelve/plugin/00-colorscheme.lua
+++
@@ -1,7 +0,0 @@
-vim.pack.add({
-	"https://github.com/luisiacc/gruvbox-baby",
-})
-
-vim.g.gruvbox_baby_transparent_mode = true
-vim.g.gruvbox_baby_background_color = "dark"
-vim.cmd.colorscheme("gruvbox-baby")
